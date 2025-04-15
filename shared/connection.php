d130511b2d5cd5c84555ee0522ff29d2babe6a36
Dianne <daramirez1205@gmail.com>
2025-04-15 09:58:41 +0800

    env

--- shared/connection.php
+++ shared/connection.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 
-$dbHost = 'localhost';
-$dbUser = 'root';
-$dbPass = '';
-$dbName = 'lafrezagold_db';
+
+$dbHost = getenv("DB_HOST") ?: "localhost";
+$dbUser = getenv("DB_USER") ?: "root";
+$dbPass = getenv("DB_PASSWORD") ?: "";
+$dbName   = getenv("DB_NAME") ?: "lafrezagold_db";
 
 // data source name
 $dsn = 'mysql:host=' . $dbHost . ';dbname=' . $dbName;
@@ -11,6 +12,6 @@ $dsn = 'mysql:host=' . $dbHost . ';dbname=' . $dbName;
 try {
     $pdo = new PDO($dsn, $dbUser, $dbPass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     die('Conection failed: ' . $e->getMessage());
 }
